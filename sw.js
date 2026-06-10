0676ae4cbdfde2645d1927b7adafd8dc29d18242
markosbad1988-lang <markos.bad1988@gmail.com>
2026-06-10 20:45:58 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'valifast-v17';
+const CACHE = 'valifast-v18';
 const ASSETS = ['/', '/index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
