2211b1e02da0e73853ff029a59a011f26d24b4a2
markosbad1988-lang <markos.bad1988@gmail.com>
2026-06-10 18:24:44 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,13 @@
-const CACHE = 'valifast-v9';
+const CACHE = 'valifast-v10';
 const ASSETS = ['/', '/index.html'];
-
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
-  );
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached)));
 });
